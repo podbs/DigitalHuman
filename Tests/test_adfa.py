72e832a2a9f95e968c281599aafe8dc5ebe7e510
guomc9 <samcolin1639@gmail.com>
2024-11-10 18:26:17 +0800

    update adfa

---
+++ Tests/test_adfa.py
@@ -0,0 +1,14 @@
+import sys
+sys.path.append('.')
+from ADFA.adfa_wrapper import ADFAWrapper
+
+if __name__ == '__main__':
+    wrapper = ADFAWrapper(config_path='ADFA/config/custom.yaml')
+    output_video_path = wrapper(
+        source_image_path="recordings/adfa/ref_image/jake.png",
+        driving_audio_path="recordings/adfa/tgt_audio/test_audio.wav",
+        output_path="recordings/adfa/gen_animate/output.mp4",
+        pose_weight=1.0,
+        face_weight=1.0,
+        lip_weight=1.0
+    )
