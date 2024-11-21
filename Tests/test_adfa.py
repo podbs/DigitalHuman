784f46632a6e41ca518c9776c533e307163d8575
Podbs <22421228@zju.edu.cn>
2024-11-21 23:33:29 +0800

    update

--- Tests/test_adfa.py
+++ Tests/test_adfa.py
@@ -4,10 +4,18 @@ from ADFA.adfa_wrapper import ADFAWrapper
 
 if __name__ == '__main__':
     wrapper = ADFAWrapper(config_path='ADFA/config/custom.yaml')
+    # output_video_path = wrapper(
+    #     source_image_path="recordings/adfa/ref_image/jake.png",
+    #     driving_audio_path="recordings/adfa/tgt_audio/test_audio.wav",
+    #     output_path="recordings/adfa/gen_animate/output.mp4",
+    #     pose_weight=1.0,
+    #     face_weight=1.0,
+    #     lip_weight=1.0
+    # )
     output_video_path = wrapper(
-        source_image_path="recordings/adfa/ref_image/jake.png",
-        driving_audio_path="recordings/adfa/tgt_audio/test_audio.wav",
-        output_path="recordings/adfa/gen_animate/output.mp4",
+        source_image_path="recordings/reference_images/user_face.jpg",
+        driving_audio_path="recordings/output_audio/output_audio.wav",
+        output_path="recordings/output_video/output.mp4",
         pose_weight=1.0,
         face_weight=1.0,
         lip_weight=1.0
--- gradio/video_processor.py
+++ gradio/video_processor.py
@@ -40,13 +40,47 @@ class VideoProcessor:
             face_location,  = face_recognition.face_locations(image)
             # print(face_location)
             top, right, bottom, left = face_location
+            
+            # 寬度（水平方向）的像素數從左到右開始計數
+            # 高度（垂直方向）的像素數從上到下開始計數     
             print("A face is located at pixel location Top: {}, Left: {}, Bottom: {}, Right: {}".format(top, left, bottom, right))
-            top -= 100
-            right += 30
+            
+            # 手動調整，確保人臉被包括到圖片内
+            top -= 150
+            bottom += 50
+            left -= 50
+            right += 50
+            length = right - left
+            width = bottom - top
+            print("length = {}, width = {}".format(length, width))
+            print("After cut out Top: {}, Left: {}, Bottom: {}, Right: {}".format(top, left, bottom, right))
+            x_shorter = 0 if length > width else 1
+            
+            if x_shorter:
+                gap = (width - length) // 2 
+                if (width - length) % 2 == 0:
+                    left -= gap
+                    right += gap
+                else:
+                    left -= gap
+                    right += (gap + 1)
+            else:
+                gap = (length - width) // 2
+                if(length - width) % 2 == 0:
+                    top -= gap
+                    bottom += gap
+                else:
+                    top -= (gap + 1)
+                    bottom += gap
+                    
+            # 查看原圖像的分辨率
+            # pil_image_original = Image.fromarray(image)
+            # print(pil_image_original.size)
             face_image = image[top:bottom, left:right]
             pil_image = Image.fromarray(face_image)
             # pil_image.show()
-            pil_image.save("./test_image1.jpg")
+            print("输出图像分辨率为：{}".format( pil_image.size))
+            pil_image.save(self.output_image_path)
         return True
     
     def video_to_audio(self, input_file, output_file):
